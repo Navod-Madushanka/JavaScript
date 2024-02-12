a47352a1653248954d7039f293f0f21d5ce16a77
Navod-Madushanka <125394445+Navod-Madushanka@users.noreply.github.com>
2024-02-12 11:09:03 +0530

    Add files via upload

---
+++ Async Await Random joke/script.js
@@ -0,0 +1,30 @@
+// API Link = https://api.chucknorris.io/jokes/random
+
+const joke = document.querySelector(".joke");
+const btn = document.querySelector(".btn");
+const url = "https://api.chucknorris.io/jokes/random";
+
+btn.addEventListener("click", getJoke);
+
+// function getJoke() {
+//     fetch(url).then((response) => {
+//         return response.json();
+//     }).then((data) => {
+//         console.log(data);
+//         joke.innerHTML = data.value;
+//     });
+// }
+
+async function getJoke() {
+    try {
+
+        const response = await fetch(url);
+        const data = await response.json();
+
+        joke.innerHTML = data.value;
+
+    } catch (error) {
+        joke.innerHTML = error.message;
+    }
+
+}
